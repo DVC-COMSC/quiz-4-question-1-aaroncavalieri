fff78c8edaa30b76e4126f46974e5d81c845efe8
Your Name/Applications/anaconda3/bin/python <you@example.com>
2023-03-05 20:50:40 -0800

    V01

--- q1.py
+++ q1.py
@@ -1,6 +1,17 @@
 
 # ******************************
-# Make your Code
+num1 = int(input())
+
+while True:
+    num2 = int(input())
+    
+    if num1 % 2 == 0 and num2 % 2 == 0:
+        print(num1, num2, end=' ')
+        break
+    
+    num1 = num2
+
+print()
 # ******************************
 
 
